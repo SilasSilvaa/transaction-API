efd5aa3093490f68ad4e39e3616a8c8a2ef5a9aa
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 22:09:41 -0300

    feat - add transaction service interface

---
+++ src/main/java/com/ssilvadev/transactionapi/services/ITransactionService.java
@@ -0,0 +1,15 @@
+package com.ssilvadev.transactionapi.services;
+
+import com.ssilvadev.transactionapi.domain.dtos.TransactionDTO;
+import com.ssilvadev.transactionapi.domain.entities.Transaction;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ITransactionService {
+    Transaction save(TransactionDTO transactionDTO);
+    List<Transaction> findAll();
+    Transaction findById(UUID id);
+    Transaction update(UUID id, TransactionDTO transactionDTO);
+    void delete(UUID id);
+}
