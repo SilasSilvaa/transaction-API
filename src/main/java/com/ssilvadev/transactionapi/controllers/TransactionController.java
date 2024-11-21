407f6b066f03a44a0ba322f16b5480026b25cc02
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 23:25:06 -0300

    feat - create post method

--- src/main/java/com/ssilvadev/transactionapi/controllers/TransactionController.java
+++ src/main/java/com/ssilvadev/transactionapi/controllers/TransactionController.java
@@ -1,9 +1,26 @@
 package com.ssilvadev.transactionapi.controllers;
 
+import com.ssilvadev.transactionapi.domain.dtos.TransactionDTO;
+import com.ssilvadev.transactionapi.domain.entities.Transaction;
+import com.ssilvadev.transactionapi.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> save(@RequestBody TransactionDTO data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService
+                .save(new TransactionDTO(data)));
+    }
 }
