cec71bc536664921ada4a5819bc9ef1f951a08f1
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 23:27:04 -0300

    feat - add properties to transactionDTO

--- src/main/java/com/ssilvadev/transactionapi/domain/dtos/TransactionDTO.java
+++ src/main/java/com/ssilvadev/transactionapi/domain/dtos/TransactionDTO.java
@@ -1,4 +1,17 @@
 package com.ssilvadev.transactionapi.domain.dtos;
 
-public record TransactionDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record TransactionDTO(
+        BigDecimal total,
+        LocalDateTime created,
+        String cardNumber,
+        String cvv,
+        String owner,
+        String ein) {
+    public TransactionDTO(TransactionDTO data) {
+        this(data.total(), data.created(), data.cardNumber(), data.cvv(), data.owner(), data.ein());
+    }
 }
+
