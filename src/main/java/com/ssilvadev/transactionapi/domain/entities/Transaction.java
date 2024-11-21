03f357158220318eaf29f4b219e05ef6f8fb1615
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 23:24:46 -0300

    feat - add properties to entity

--- src/main/java/com/ssilvadev/transactionapi/domain/entities/Transaction.java
+++ src/main/java/com/ssilvadev/transactionapi/domain/entities/Transaction.java
@@ -2,13 +2,30 @@ package com.ssilvadev.transactionapi.domain.entities;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "transactions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID transactionId;
+
+    private BigDecimal total;
+    private LocalDateTime created;
+    private String cardNumber;
+    private String cvv;
+    private String owner;
+    private String ein;
 }
