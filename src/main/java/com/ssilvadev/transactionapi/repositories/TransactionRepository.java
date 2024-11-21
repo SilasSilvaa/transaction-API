860bbd443731603b2c90288c654ce1eae5577dd3
SilasSilvaa <silas-oli@hotmail.com>
2024-11-20 23:25:43 -0300

    refactor - add annotation @Repository

--- src/main/java/com/ssilvadev/transactionapi/repositories/TransactionRepository.java
+++ src/main/java/com/ssilvadev/transactionapi/repositories/TransactionRepository.java
@@ -2,8 +2,10 @@ package com.ssilvadev.transactionapi.repositories;
 
 import com.ssilvadev.transactionapi.domain.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 }
