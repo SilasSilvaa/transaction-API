923110dc8da241863c3ec5983e300d9d66cbc7a4
SilasSilvaa <silas-oli@hotmail.com>
2024-11-23 11:35:26 -0300

    feat - add methods to find, update and get transaction by id

--- src/main/java/com/ssilvadev/transactionapi/controllers/TransactionController.java
+++ src/main/java/com/ssilvadev/transactionapi/controllers/TransactionController.java
@@ -3,24 +3,42 @@ package com.ssilvadev.transactionapi.controllers;
 import com.ssilvadev.transactionapi.domain.dtos.TransactionDTO;
 import com.ssilvadev.transactionapi.domain.entities.Transaction;
 import com.ssilvadev.transactionapi.services.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
 
-    @Autowired
-    private TransactionService transactionService;
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService){
+        this.transactionService = transactionService;
+    }
 
     @PostMapping
     public ResponseEntity<Transaction> save(@RequestBody TransactionDTO data) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionService
                 .save(new TransactionDTO(data)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> findById(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> update(@PathVariable UUID id, @RequestBody TransactionDTO data){
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.update(id, data));
     }
 }
