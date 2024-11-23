6a4c407fffc5d2aad83cc96cc868561f31783063
SilasSilvaa <silas-oli@hotmail.com>
2024-11-23 11:36:02 -0300

    feat - add service methods to find, update and get transaction by id

--- src/main/java/com/ssilvadev/transactionapi/services/TransactionService.java
+++ src/main/java/com/ssilvadev/transactionapi/services/TransactionService.java
@@ -4,21 +4,25 @@ import com.ssilvadev.transactionapi.domain.dtos.TransactionDTO;
 import com.ssilvadev.transactionapi.domain.entities.Transaction;
 import com.ssilvadev.transactionapi.repositories.TransactionRepository;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TransactionService implements ITransactionService {
 
-    @Autowired
-    private TransactionRepository transactionRepository;
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository){
+        this.transactionRepository = transactionRepository;
+    }
 
     @Override
     public Transaction save(TransactionDTO transactionDTO) {
         Transaction transaction = new Transaction();
+
         BeanUtils.copyProperties(transactionDTO, transaction);
 
         return transactionRepository.save(transaction);
@@ -26,21 +30,31 @@ public class TransactionService implements ITransactionService {
 
     @Override
     public List<Transaction> findAll() {
-        return List.of();
+        return transactionRepository.findAll();
     }
 
     @Override
     public Transaction findById(UUID id) {
-        return null;
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+
+        if(transaction.isEmpty()){
+            throw new RuntimeException("Transaction not found");
+        }
+
+        return transaction.get();
     }
 
     @Override
     public Transaction update(UUID id, TransactionDTO transactionDTO) {
-        return null;
+        Optional<Transaction> transaction =  transactionRepository.findById(id);
+
+        if(transaction.isEmpty()){
+            throw new RuntimeException("Transaction not found");
+        }
+
+        BeanUtils.copyProperties(transactionDTO, transaction.get());
+
+        return transactionRepository.save(transaction.get());
     }
 
-    @Override
-    public void delete(UUID id) {
-
-    }
 }
